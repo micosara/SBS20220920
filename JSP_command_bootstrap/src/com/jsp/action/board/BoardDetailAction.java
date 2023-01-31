7e2d31cd35bb29d8f589dc7092832b9ed869711f
micosara <micosara365@gmail.com>
2023-01-31 12:15:38 +0900

    JSP_command_bootstrap : board detail

---
+++ JSP_command_bootstrap/src/com/jsp/action/board/BoardDetailAction.java
@@ -0,0 +1,37 @@
+package com.jsp.action.board;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jsp.action.Action;
+import com.jsp.dto.BoardVO;
+import com.jsp.service.BoardService;
+
+public class BoardDetailAction implements Action{
+
+	private BoardService boardService;
+	public void setBoardService(BoardService boardService) {
+		this.boardService = boardService;
+	}	
+	
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String url = "/board/detail";
+		
+		int bno = Integer.parseInt(request.getParameter("bno"));
+		
+		BoardVO board=boardService.getBoard(bno);		
+
+		request.setAttribute("board", board);
+		
+		return url;
+	}
+
+}
+
+
+
+
+
+
+
