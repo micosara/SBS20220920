8d46ce981d9dfef9ef801d7fcc1c7365ba36582c
micosara <micosara365@gmail.com>
2023-03-03 11:39:42 +0900

    spring_bootstrap : add loginUserLog to database scheduler

---
+++ spring_bootstrap/src/main/java/kr/ac/sbs/dao/LoginUserLogDAO.java
@@ -0,0 +1,10 @@
+package kr.ac.sbs.dao;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+public interface LoginUserLogDAO {
+
+	public void insertLoginUserLog(Map<String,Object> logVO)throws SQLException;
+	public void deleteLoginUserLog()throws SQLException;
+}
